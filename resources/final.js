7940bb27ed70feb42d1e649ec2605a9adc01b495
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-04-23 00:03:20 -0400

    fixing column headers

--- resources/final.js
+++ resources/final.js
@@ -1,44 +1,44 @@
 var config = {
   "title": "Final Exam Jeopardy",
-  "columns": ["windows7","word","excel","access","powerpoint"],
+  "columns": ["Windows7","Word","Excel","Access","Powerpoint"],
   "example_file": {"file":"", "text":""}
 }
 var theData = {
   "one": {
-    "windows7": "In Windows 7 the ____ is a strip that contains buttons to give you quick access to common tools and running programs.",
-    "word": "To delete only the contents of the table, but not the table itself, select the contents and ____.",
-    "excel": "The intersection of a row and column is known as a ____.",
-    "access": "The first step in organizing data into a database is to identify the individual ____.",
-    "powerpoint": "What helps a presenter to remember what to say when a particular slide appears during the presentation?"
+    "one": "In Windows 7 the ____ is a strip that contains buttons to give you quick access to common tools and running programs.",
+    "two": "To delete only the contents of the table, but not the table itself, select the contents and ____.",
+    "three": "The intersection of a row and column is known as a ____.",
+    "four": "The first step in organizing data into a database is to identify the individual ____.",
+    "five": "What helps a presenter to remember what to say when a particular slide appears during the presentation?"
   },
   "two":{
-    "windows7": "The Start menu is organized into two ____, or separate areas of a menu or window.",
-    "word": "To create a footnote, click Insert Footnote in the Footnotes group in the ____ tab.",
-    "excel": "Italic, bold, bold italic, and underline are examples of ____.",
-    "access": "A table’s contents are displayed as rows and columns in ____.",
-    "powerpoint": "When text is added to the placeholder in PowerPoint, the ___ feature changes the line spacing and the font size of the text to fit in the placeholder."
+    "one": "The Start menu is organized into two ____, or separate areas of a menu or window.",
+    "two": "To create a footnote, click Insert Footnote in the Footnotes group in the ____ tab.",
+    "three": "Italic, bold, bold italic, and underline are examples of ____.",
+    "four": "A table’s contents are displayed as rows and columns in ____.",
+    "five": "When text is added to the placeholder in PowerPoint, the ___ feature changes the line spacing and the font size of the text to fit in the placeholder."
   },
   "three": {
-    "windows7": "____ makes it easy to navigate from one location to another within the file hierarchy.",
-    "word": "A special design element in Microsoft Word, called ____ draws the reader’s attention to the beginning of a paragraph or a document.",
-    "excel": "When using the Format Painter, double-click the Format Painter button to paste the same format multiple times and click the Format Painter again to ____.",
-    "access": "Storing the same data field in multiple tables is called data ____.",
-    "powerpoint": "Which of PowerPoint's built-in layouts doesn't contain content placeholders?"
+    "one": "____ makes it easy to navigate from one location to another within the file hierarchy.",
+    "two": "A special design element in Microsoft Word, called ____ draws the reader’s attention to the beginning of a paragraph or a document.",
+    "three": "When using the Format Painter, double-click the Format Painter button to paste the same format multiple times and click the Format Painter again to ____.",
+    "four": "Storing the same data field in multiple tables is called data ____.",
+    "five": "Which of PowerPoint's built-in layouts doesn't contain content placeholders?"
 
   },
   "four": {
-    "windows7": "A(n) ____ is a row of buttons on the title bar that let you perform common tasks such as saving a file and undoing an action.",
-    "word": "To create a numbered list, you use the Numbering button in the ____ group.",
-    "excel": "Anya needs to call attention to one category in her chart above all others. For this purpose, she should use a(n) ____.",
-    "access": "Martha knows that if she is selecting fields using a condition, she does not need to use the default operator because it is optional. Which of the following is the default operator?",
-    "powerpoint": "How do you know a text box is active? (Hint: It has to do with the border.)"
+    "one": "A(n) ____ is a row of buttons on the title bar that let you perform common tasks such as saving a file and undoing an action.",
+    "two": "To create a numbered list, you use the Numbering button in the ____ group.",
+    "three": "Anya needs to call attention to one category in her chart above all others. For this purpose, she should use a(n) ____.",
+    "four": "Martha knows that if she is selecting fields using a condition, she does not need to use the default operator because it is optional. Which of the following is the default operator?",
+    "five": "How do you know a text box is active? (Hint: It has to do with the border.)"
   },
   "five": {
-    "windows7": "On the Ribbon, a ____ control is highlighted when it is turned on.",
-    "word": "Name all of the wrapping styles for graphics? (Hint: Think about how text can wrap around pictures.)",
-    "excel": "When you copy a selection of cells using drag and drop, which appears before you release the mouse button?",
-    "access": "List the steps to delete a record.",
-    "powerpoint": "List all the ways available for printing handouts of your presentation."
+    "one": "On the Ribbon, a ____ control is highlighted when it is turned on.",
+    "two": "Name all of the wrapping styles for graphics? (Hint: Think about how text can wrap around pictures.)",
+    "three": "When you copy a selection of cells using drag and drop, which appears before you release the mouse button?",
+    "four": "List the steps to delete a record.",
+    "five": "List all the ways available for printing handouts of your presentation."
 
   }
 }
