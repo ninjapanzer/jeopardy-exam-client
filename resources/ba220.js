c0b10e62f44cef901351251dd3b30f1f306da77d
Savannah Butler <savannahscarrone@gmail.com>
2014-04-29 10:55:17 -0400

    Rearranged game questions

--- resources/ba220.js
+++ resources/ba220.js
@@ -4,24 +4,18 @@ var config = {
   "example_file": {"file":"", "text":""}
 }
 var theData = {
-  "one": {
-<<<<<<< HEAD
+  "one":{
     "one": "Your company wants to increase market share.",
     "two": "<img src = './resources/images/breyers.jpg'/>",
     "three": "What objective would you use if the market wasn't growing?",
-=======
-    "one": "<img src = './resources/images/breyers.jpg'/>",
-    "two": "",
-    "three": "",
->>>>>>> 02624158b8342d8ee035a565d8ef5e68ee101590
   },
   "two":{
     "one": "Price policies usually lead to _________.",
     "two": "<img src = './resources/images/carnival.jpg'/>",
-    "three": "<img src = './resources/images/forceflex.jpg'/>",
+    "three": "<img src = '.resources/images/forceflex.jpg'/>",
   },
   "three": {
-    "one": "What are some different names for price? (Name at least two.)",
+    "one": "What is the price equation?",
     "two": "What are the three main price objectives?",
     "three": "Name a disadvantage to flexible pricing.",
   },
