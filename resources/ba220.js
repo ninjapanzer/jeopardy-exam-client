d45121c8bd93b728f0b0d001f2e99fcdf4750974
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-04-29 09:48:45 -0400

    resolved conflict merge and linted

--- resources/ba220.js
+++ resources/ba220.js
@@ -2,27 +2,22 @@ var config = {
   "title": "Is the Price Right? Jeopardy",
   "columns": ["Name that Objective","Name that Policy","Misc."],
   "example_file": {"file":"", "text":""}
-}
+};
+
 var theData = {
   "one": {
-<<<<<<< HEAD
     "one": "Your company wants to increase market share.",
     "two": "<img src = './resources/images/breyers.jpg'/>",
-    "three": "What objective would you use if the market wasn't growing?",
-=======
-    "one": "<img src = './resources/images/breyers.jpg'/>",
-    "two": "",
-    "three": "",
->>>>>>> 02624158b8342d8ee035a565d8ef5e68ee101590
+    "three": "What objective would you use if the market wasn't growing?"
   },
   "two":{
     "one": "Price policies usually lead to _________.",
     "two": "<img src = './resources/images/carnival.jpg'/>",
-    "three": "<img src = './resources/images/forceflex.jpg'/>",
+    "three": "<img src = './resources/images/forceflex.jpg'/>"
   },
   "three": {
     "one": "What are some different names for price? (Name at least two.)",
     "two": "What are the three main price objectives?",
-    "three": "Name a disadvantage to flexible pricing.",
+    "three": "Name a disadvantage to flexible pricing."
   },
 }
