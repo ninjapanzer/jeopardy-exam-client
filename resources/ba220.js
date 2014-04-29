6319d4b92907ee4274d74cb838434f2b029a10ce
Savannah Butler <savannahscarrone@gmail.com>
2014-04-29 09:40:28 -0400

    Updated game data

--- resources/ba220.js
+++ resources/ba220.js
@@ -1,22 +1,22 @@
 var config = {
-  "title": "Final Exam Jeopardy",
+  "title": "Is the Price Right? Jeopardy",
   "columns": ["Name that Objective","Name that Policy","Misc."],
   "example_file": {"file":"", "text":""}
 }
 var theData = {
   "one": {
-    "one": "<img src = 'images/breyers.jpg/>'",
-    "two": "",
-    "three": "",
+    "one": "Your company wants to increase market share.",
+    "two": "<img src = 'resources/images/breyers.jpg/>'",
+    "three": "What objective would you use if the market wasn't growing?",
   },
   "two":{
-    "one": "",
-    "two": "",
-    "three": "",
+    "one": "Price policies usually lead to _________.",
+    "two": "<img src = 'resources/images/carnival.jpg/>",
+    "three": "<img src = 'resources/images/forceflex.jpg/>",
   },
   "three": {
-    "one": "",
-    "two": "",
-    "three": "",
+    "one": "What are some different names for price? (Name at least two.)",
+    "two": "What are the three main price objectives?",
+    "three": "Name a disadvantage to flexible pricing.",
   },
 }
