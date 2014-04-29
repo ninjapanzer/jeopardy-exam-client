b78a40d87ce2d51e57413015a951d39e7de42d0c
Savannah Butler <savannahscarrone@gmail.com>
2014-04-29 09:06:11 -0400

    intermediate files for testing

---
+++ resources/ba220.js
@@ -0,0 +1,22 @@
+var config = {
+  "title": "Final Exam Jeopardy",
+  "columns": ["Name that Objective","Name that Policy","Misc."],
+  "example_file": {"file":"", "text":""}
+}
+var theData = {
+  "one": {
+    "one": "<img src = 'images/breyers.jpg/>'",
+    "two": "",
+    "three": "",
+  },
+  "two":{
+    "one": "",
+    "two": "",
+    "three": "",
+  },
+  "three": {
+    "one": "",
+    "two": "",
+    "three": "",
+  },
+}
